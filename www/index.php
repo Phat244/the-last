6e912638fc38742a122db2a6d5e63d2da3bb3269
Phat244 <71125470+Phat244@users.noreply.github.com>
2020-11-06 18:48:07 +0700

    Add files via upload

--- www/index.php
+++ www/index.php
@@ -1,67 +1,106 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Lab</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <style>
-            p{
-                color: slategrey;
-            }
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="63070244.css" rel="stylesheet">
 
-            h5{
-                color: black;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="card-deck">
-                <div class="card">
-                  <img src="work.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Worker</h5>
-                    <p class="card-text">a person who does a specified type of work or who works in a specified way.</p>
-                  </div>
-                  <div class="card-footer">
-                    <img style="border-radius: 50%;" src="img_avatar.png" alt="Avatar" height=25>
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="green" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                      </svg>
-                  </div>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row row-height">
+            <div class="col-md-6 cell">
+                <div class="cell-body">
+                    <h3>Column_001</h3>
+                    <p>Well, I will call you darlin' and everything will be okay<br>
+                        'Cause I know that I am yours and you are mine<br>
+                        Doesn't matter anyway<br>
+                        In the night, we'll take a walk, it's nothing funny<br>
+                        Just to talk</p>
+                    <img src="003.jpg" style = "height: 40%;, width: 40%;" class="img-fluid" />
                 </div>
-                <div class="card">
-                  <img src="work4.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Tree</h5>
-                    <p class="card-text">a tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves in most species.</p>
-                  </div>
-                  <div class="card-footer">
-                    <img style="border-radius: 50%;" src="img_avatar.png" alt="Avatar" height=25>
-                    <small class="text-muted">Last updated 8 mins ago</small>
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="green" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                      </svg>
-                  </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row height-100">
+                    <div class="col-sm-6">
+                        <div class="row height-100">
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body">
+                                    <h3>Column_002</h3>
+                                    <p>Put your hand in mine<br>
+                                        You know that I want to be with you all the time<br>
+                                        You know that I won't stop until I make you mine<br>
+                                        You know that I won't stop until I make you mine<br>
+                                        Until I make you mine</p>
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body">
+                                    <h3>Column_003</h3>
+                                    <p>Well, I have called you darlin' and I'll say it again, again<br>
+                                        So kiss me 'til I'm sorry, babe, that you are gone and I'm a mess<br>
+                                        And I'll hurt you and you'll hurt me and we'll say things we can't repeat</p>
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body">
+                                    <h3>Column_004</h3>
+                                    <p>Put your hand in mine<br>
+                                        You know that I want to be with you all the time<br>
+                                        You know that I won't stop until I make you mine<br>
+                                        You know that I won't stop until I make you mine<br>
+                                        Until I make you mine</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row height-100">
+                            <div class="col-12 cell" style="height: 66%;">
+                                <div class="cell-body">
+                                    <h3>Column_005</h3>
+                                    <p>You need to know<br>
+                                        We'll take it slow<br>
+                                        I miss you so<br>
+                                        We'll take it slow<br>
+                                        It's hard to feel you slipping (You need to know)<br>
+                                        Through my fingers are so numb (We'll take it slow)<br>
+                                        And how was I supposed to know (I miss you so)<br>
+                                        That you were not the one?<br>
+                                        Put your hand in mine<br>
+                                        You know that I want to be with you all the time<br>
+                                        You know that I won't stop until I make you mine<br>
+                                        You know that I won't stop until I make you mine<br>
+                                        Until I make you mine<br>
+                                        Put your hand in mine<br>
+                                        You know that I want to be with you all the time<br>
+                                        Oh darlin', darlin', baby, you're so very fine<br>
+                                        You know that I won't stop until I make you mine<br>
+                                        Until I make you</p>
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 34%;">
+                                <div class="cell-body">
+                                    <h3>Column_006</h3>
+                                    <P>La-la-la-la<br>
+                                        La-la-la-la-la-la<br>
+                                        La-la-la-la-la<br>
+                                        La-la-la-la-la-la<br>
+                                        La-la-la-la-la<br>
+                                        La-la-la-la-la-la<br>
+                                        La-la-la-la-la</P>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
-                <div class="card">
-                  <img src="work3.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Work</h5>
-                    <p class="card-text">work is the energy transferred to or from an object via the application of force along a displacement. In its simplest form.</p>
-                  </div>
-                  <div class="card-footer">
-                    <img style="border-radius: 50%;" src="img_avatar.png" alt="Avatar" height=25>
-                    <small class="text-muted">Last updated 10 mins ago</small>
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="green" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                      </svg>
-                  </div>
-                </div>
-              </div>
-          </div>
-    </body>
+            </div>
+        </div>
+        
+    </div>
+</body>
 </html>
